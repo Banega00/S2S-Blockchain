a3b176a8d59b2ab3c5e9821694e264b521e618c5
Branislav_Stojanovic <branislav.stojanovic@blinking.id>
2022-10-15 18:21:57 +0200

    config stuff

---
+++ miner/frontend.js
@@ -0,0 +1,10 @@
+const MINE_RATE = 1000*1000 //1000 sec
+const INITIAL_DIFFICULTY = 12;
+
+const GENESIS_DATA = {
+    timestamp: 1,
+    previousHash: '-----',
+    hash: 'hash-one',
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0
+}
