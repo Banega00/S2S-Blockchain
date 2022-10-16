4c9d6def17fec363b5bce29d42e5412f108bedcf
Branislav_Stojanovic <branislav.stojanovic@blinking.id>
2022-10-16 13:58:13 +0200

    transaction class

--- miner/frontend.js
+++ miner/frontend.js
@@ -131,7 +131,7 @@ class Wallet{
         console.log('Keys generated')
     }
 
-    sing(data){
+    sign(data){
         const digSign = this.keyPair.sign(calculateHash(concatAndStringify(data)));
         return digSign;
     }
@@ -261,6 +261,90 @@ class Block{
 
         return difficulty+1;
     }
+
+    getHash(){
+        return calculateHash(concatAndStringify
+            (this.timestamp, this.previousHash, this.data, this.nonce, this.difficulty))
+    }
+}
+
+class Transaction {
+    id;
+    outputMap;
+    input;
+
+    constructor(obj) {
+        const { id, senderWallet, recipient, amount, outputMap, input } = obj;
+        this.id = id ?? uuidv4()
+        this.outputMap = outputMap || this.createOutputMap({ senderWallet, recipient, amount });
+        this.input = input || this.createInput({ senderWallet, outputMap: this.outputMap });
+
+    }
+
+    createOutputMap({ senderWallet, recipient, amount }) {
+        const outputMap = {};
+
+        outputMap[recipient] = amount;
+        outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
+
+        return outputMap;
+    }
+
+    createInput({ senderWallet, outputMap }) {
+        return {
+            timestamp: Date.now(),
+            amount: senderWallet.balance,
+            address: senderWallet.publicKey,
+            signature: senderWallet.sign(outputMap)
+        };
+    }
+
+    update({ senderWallet, recipient, amount }) {
+        if (amount > this.outputMap[senderWallet.publicKey]) {
+            throw new Error('Amount exceeds balance');
+        }
+
+        if (!this.outputMap[recipient]) {
+            this.outputMap[recipient] = amount;//if recipient is new
+        } else {
+            this.outputMap[recipient] = this.outputMap[recipient] + amount;//if same recipient already exists in transaction
+        }
+
+        this.outputMap[senderWallet.publicKey] =
+            this.outputMap[senderWallet.publicKey] - amount;//reduce sender remaining value
+
+        this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
+    }
+
+    static validTransaction(transaction) {
+        const { input: { address, amount, signature }, outputMap } = transaction;
+
+        const outputTotal = Object.values(outputMap)
+            .reduce((total, outputAmount) => total + outputAmount);
+
+        if (amount !== outputTotal) {
+            console.error(`Invalid transaction from ${address}`);
+            return false;
+        }
+
+        if (!verifySignature({ publicKey: address, data: outputMap, signature })) {
+            console.error(`Invalid signature from ${address}`);
+            return false;
+        }
+
+        return true;
+    }
+
+    static rewardTransaction(obj) {
+        const { minerWallet } = obj;
+        return new this({
+            input: REWARD_INPUT,
+            outputMap: { [minerWallet.publicKey]: MINING_REWARD }
+        });
+    }
 }
 
 
+class Blockchain{
+    chain; //array of blocks
+}
